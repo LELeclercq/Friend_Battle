545539e1b28ab42976e80930bc41478f6da1dd1c
Nick Andryshak <nick.lanshack@gmail.com>
2014-06-13 12:27:05 -0400

    Made the file actually readable Only took 3 Emacs commands

--- character.py
+++ character.py
@@ -5,16 +5,30 @@ pygame.init()
 import time
 
 
-luke_stats = {'name': 'Lucas', 'hp': 20, 'mp': 5, 'atkstr': range(1, 3),
-              'intro_quote': 'Time to get blasted!', 'outro_quote': 'Guys, I\'m just gonna go walk home.', 'music': 'Lucas_battle.mp3',
-              'bg': 'Lucas_bg.jpg'}
+luke_stats = {
+    'name': 'Lucas', 
+    'hp': 20, 
+    'mp': 5, 
+    'atkstr': range(1, 3),
+    'intro_quote': 'Time to get blasted!',
+    'outro_quote': 'Guys, I\'m just gonna go walk home.',
+    'music': 'Lucas_battle.mp3',
+    'bg': 'Lucas_bg.jpg'
+}
 
-luke_quotes = ['Lucas complains at you until your ears bleed!', 'Lucas beaches you to death!']
+luke_quotes = ['Lucas complains at you until your ears bleed!', 
+               'Lucas beaches you to death!']
 
-dom_stats = {'name': 'Dom', 'hp': 40, 'mp': 20, 'atkstr': range(3, 6),
-             'intro_quote': 'How do you play this game again?',
-             'outro_quote': 'Whatever, I\'m just gonna move to North Carolina', 'music': 'Dom_battle.mp3',
-             'bg': 'Dom_bg.jpg'}
+dom_stats = {
+    'name': 'Dom', 
+    'hp': 40, 
+    'mp': 20, 
+    'atkstr': range(3, 6),
+    'intro_quote': 'How do you play this game again?',
+    'outro_quote': 'Whatever, I\'m just gonna move to North Carolina',
+    'music': 'Dom_battle.mp3',
+    'bg': 'Dom_bg.jpg'
+}
 
 dom_quotes = ['Dom stumbles into you, mumbling apologies and mentioning something about too much to drink.',
               'Dom locks you into a bearhug, squeezing the life out of you while mentioning how much he loves you, man.',
@@ -22,52 +36,110 @@ dom_quotes = ['Dom stumbles into you, mumbling apologies and mentioning somethin
               'Dom pukes up a ton of blood on you. Like, a metric ton of blood. Then he passes out on the floor.',
               'Dom uses his drunk strength to sock you in the stomach. You fly at least a foot off the ground.']
 
-jim_stats = {'name': 'Jim', 'hp': 30, 'mp': 40, 'atkstr': range(4, 7),
-              'intro_quote': 'You know I love me the big game!', 'outro_quote': 'Should\'ve stuck to watching animu...', 'music': 'Jim_battle.mp3',
-              'bg': 'Jim_bg.jpg'}
+jim_stats = {
+    'name': 'Jim',
+    'hp': 30,
+    'mp': 40,
+    'atkstr': range(4, 7),
+    'intro_quote': 'You know I love me the big game!',
+    'outro_quote': 'Should\'ve stuck to watching animu...',
+    'music': 'Jim_battle.mp3',
+    'bg': 'Jim_bg.jpg'
+}
 
-jim_quotes = ['Jim blows out your ears with a concentrated blast of deadmau5!', 'Jim smothers you with his Senjougahara dakimakura!', 'Final Spark: After gathering energy for half a second, Jim fires a broad and long-range\nbeam of light in a line that deals magic damage!', 'Jim calls out his stand \'Knife Party\', causing a shower of knives to rain from the sky!']
+jim_quotes = ['Jim blows out your ears with a concentrated blast of deadmau5!',
+              'Jim smothers you with his Senjougahara dakimakura!',
+              'Final Spark: After gathering energy for half a second, Jim fires a broad and long-range\nbeam of light in a line that deals magic damage!',
+              'Jim calls out his stand \'Knife Party\', causing a shower of knives to rain from the sky!']
 
-jeff_stats = {'name': 'Jeff', 'hp': 30, 'mp': 40, 'atkstr': range(4, 7),
-              'intro_quote': 'You know I love me the big game!', 'outro_quote': '.', 'music': 'Jim_battle.mp3',
-              'bg': 'Jim_bg.jpg'}
+jeff_stats = {'name': 'Jeff',
+              'hp': 30,
+              'mp': 40,
+              'atkstr': range(4, 7),
+              'intro_quote': 'You know I love me the big game!',
+              'outro_quote': '.',
+              'music': 'Jim_battle.mp3',
+              'bg': 'Jim_bg.jpg'
+}
 
 jeff_quotes = []
 
-marsh_stats = {'name': 'Marsh', 'hp': 20, 'mp': 5, 'atkstr': range(1, 3),
-              'intro_quote': 'Time to get blasted!', 'outro_quote': 'Guys, I\'m just gonna go walk home.', 'music': 'Lucas_battle.mp3',
-              'bg': 'Lucas_bg.jpg'}
+marsh_stats = {'name': 'Marsh',
+               'hp': 20,
+               'mp': 5,
+               'atkstr': range(1, 3),
+               'intro_quote': 'Time to get blasted!',
+               'outro_quote': 'Guys, I\'m just gonna go walk home.',
+               'music': 'Lucas_battle.mp3',
+               'bg': 'Lucas_bg.jpg'
+}
 
 marsh_quotes = []
 
-shane_stats = {'name': 'Shane', 'hp': 20, 'mp': 5, 'atkstr': range(1, 3),
-              'intro_quote': 'Time to get blasted!', 'outro_quote': 'Guys, I\'m just gonna go walk home.', 'music': 'Lucas_battle.mp3',
-              'bg': 'Lucas_bg.jpg'}
+shane_stats = {'name': 'Shane',
+               'hp': 20,
+               'mp': 5,
+               'atkstr': range(1, 3),
+               'intro_quote': 'Time to get blasted!',
+               'outro_quote': 'Guys, I\'m just gonna go walk home.',
+               'music': 'Lucas_battle.mp3',
+               'bg': 'Lucas_bg.jpg'
+}
 
 shane_quotes = []
 
-nanny_stats = {'name': 'Lucas', 'hp': 20, 'mp': 5, 'atkstr': range(1, 3),
-              'intro_quote': 'Time to get blasted!', 'outro_quote': 'Guys, I\'m just gonna go walk home.', 'music': 'Lucas_battle.mp3',
-              'bg': 'Lucas_bg.jpg'}
+nanny_stats = {'name': 'Lucas',
+               'hp': 20,
+               'mp': 5,
+               'atkstr': range(1, 3),
+               'intro_quote': 'Time to get blasted!',
+               'outro_quote': 'Guys, I\'m just gonna go walk home.',
+               'music': 'Lucas_battle.mp3',
+               'bg': 'Lucas_bg.jpg'
+}
 
 nanny_quotes = []
 
-chris_stats = {'name': 'Chris', 'hp': 20, 'mp': 5, 'atkstr': range(1, 3),
-              'intro_quote': 'Time to get blasted!', 'outro_quote': 'Guys, I\'m just gonna go walk home.', 'music': 'Lucas_battle.mp3',
-              'bg': 'Lucas_bg.jpg'}
+chris_stats = {'name': 'Chris',
+               'hp': 20,
+               'mp': 5,
+               'atkstr': range(1, 3),
+               'intro_quote': 'Time to get blasted!',
+               'outro_quote': 'Guys, I\'m just gonna go walk home.',
+               'music': 'Lucas_battle.mp3',
+               'bg': 'Lucas_bg.jpg'
+}
 
 chris_quotes = []
 
-dave_stats = {'name': 'Dave', 'hp': 20, 'mp': 5, 'atkstr': range(1, 3),
-              'intro_quote': 'Time to get blasted!', 'outro_quote': 'Guys, I\'m just gonna go walk home.', 'music': 'Lucas_battle.mp3',
-              'bg': 'Lucas_bg.jpg'}
+dave_stats = {'name': 'Dave',
+              'hp': 20,
+              'mp': 5,
+              'atkstr': range(1, 3),
+              'intro_quote': 'Time to get blasted!',
+              'outro_quote': 'Guys, I\'m just gonna go walk home.',
+              'music': 'Lucas_battle.mp3',
+              'bg': 'Lucas_bg.jpg'
+}
 
 dave_quotes = []
 
-hero_stats = {'name': 'Lawrence', 'hp': 20, 'mp': 10, 'atkstr': range(4, 7),
-        'intro_quote': 'none', 'outro_quote': 'none', 'music': 'none', 'bg': 'none'}
+hero_stats = {'name': 'Lawrence',
+              'hp': 20,
+              'mp': 10,
+              'atkstr': range(4, 7),
+              'intro_quote': 'none',
+              'outro_quote': 'none',
+              'music': 'none',
+              'bg': 'none'
+}
 
-hero_quotes = ['Lawrence says the opponent took damage, and Lawrence\'s always right, so that means he must have!', 'Lawrence strkes at precisely the right angle!', 'Lawrence prattles on and on for hours talking about Zelda!', 'Lawrence channels his love of animation into a really nice fountain pen and then stabs it into his opponent!', 'Lawrence delivers a roundhouse kick to the head!', 'Lawrence spends a full hour on the finer points of JoJo\'s Bizarre Adventure exaplaining \neach character in depth as well as his disdain for the english translations.\nEventually his opponent just punches himself in the face to get it over with.']
+hero_quotes = ['Lawrence says the opponent took damage, and Lawrence\'s always right, so that means he must have!',
+               'Lawrence strkes at precisely the right angle!',
+               'Lawrence prattles on and on for hours talking about Zelda!',
+               'Lawrence channels his love of animation into a really nice fountain pen and then stabs it into his opponent!',
+               'Lawrence delivers a roundhouse kick to the head!',
+               'Lawrence spends a full hour on the finer points of JoJo\'s Bizarre Adventure exaplaining \neach character in depth as well as his disdain for the english translations.\nEventually his opponent just punches himself in the face to get it over with.']
 
 class Character(object):
 
